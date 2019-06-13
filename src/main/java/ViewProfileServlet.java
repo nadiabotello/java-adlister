787f237c33a1f094f4b9248716a000472d5eade9
Nadia Botello <botello.nadia.g@gmail.com>
2019-06-13 11:23:24 -0500

    completed sessions and cookies exercise

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
+        Boolean user = (boolean) request.getSession().getAttribute("user");
+
+        if (!user) {
+            response.sendRedirect("/login");
+            return;
+        } else {
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+        }
+
     }
+
 }
